feab3e04898251789d24bebe4514b80c9e537de6
Akhilesh Bisht <150502170+akhilesh-bisht@users.noreply.github.com>
2025-05-10 12:25:34 +0530

    some error on env

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const ConnectDb = async () => {
+const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log("MongoDB connected successfully.");
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
-    console.error("MongoDB connection error:", error.message);
+    console.log("MONGODB connection FAILED ", error);
     process.exit(1);
   }
 };
 
-export default ConnectDb;
+export default connectDB;
