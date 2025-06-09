05c8f7c616ef51a8d9ae72f45fd4f5c598367b86
Akhilesh Bisht <akhileshbisht2020@gmail.com>
2025-06-09 23:26:59 +0530

    Add getChannelDetails endpoint to fetch channel information and subscriber counts

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -331,3 +331,70 @@ export const updateCoverImage = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
+
+// get channel details
+
+export const getChannelDetails = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is required");
+  }
+
+  const channelDetails = await User.aggregate([
+    {
+      $match: {
+        username: username.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions", 
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+     {
+      $lookup: {
+        from: "subscriptions", 
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribersTo",
+      },
+
+    },
+    {
+      $addFields:{
+        subscriberCount: { $size: "$subscribers" },
+        channelSubscribersToCount: { $size: "$subscribersTo" },
+      },
+      isSubscribe:{
+        $cond:{
+          if:{$in:[req.user._id,"$subscribers.subscriber"]},
+          then:true,
+          else:false
+
+        }
+      }
+    },
+    {
+      $project: {
+       fullName: 1,
+        username: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscriberCount: 1,
+        channelSubscribersToCount: 1,
+        isSubscribe: 1, 
+      },
+    },
+  ]);
+
+  if (!channelDetails || channelDetails.length === 0) {
+    throw new ApiError(404, "Channel not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channelDetails[0], "Channel details fetched successfully"));
+});
